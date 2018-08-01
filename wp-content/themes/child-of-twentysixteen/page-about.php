bb356ca1015dcd258e054b03ec1291076be3f809
Christine McGlade <cmcglade825@gmail.com>
2018-08-01 10:45:51 -0400

    installed gutenberg plugin and google analytcis tracking code in header.php file

--- wp-content/themes/child-of-twentysixteen/page-about.php
+++ wp-content/themes/child-of-twentysixteen/page-about.php
@@ -20,7 +20,7 @@ get_header(); ?>
 		while ( have_posts() ) : the_post();
 
 			// Include the page content template.
-			get_template_part( 'template-parts/content', 'page' );
+			//get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
@@ -37,5 +37,5 @@ get_header(); ?>
 
 </div><!-- .content-area -->
 
-<?php //get_sidebar(); ?> <!--commenting out the sidebar on the homepage-->
-<?php get_footer(); ?>
+<?php //get_sidebar(); ?>
+<?php //get_footer(); ?>
